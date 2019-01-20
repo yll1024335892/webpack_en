9c35c302f13e146540bad92fece41af973f581c7
yll1024335892 <yll1024335892@163.com>
2019-01-20 13:45:57 +0800

    添加了一个react-router-dom

--- src/app.jsx
+++ src/app.jsx
@@ -1,35 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter as Router ,Route,Link,Switch} from 'react-router-dom';
 
 import './index.css';
 import './index.scss';
 
-
-class Component extends React.Component{
+class A extends React.Component{
     constructor(props){
         super(props);
-        this.state={
-            num:1
-        };
     }
-    eventClick() {
-        this.setState({
-            num:this.state.num+=1
-        });
-    }
-    render (){
-        return (<div>
-                    <h1>number : {this.state.num}</h1>
-                    <button onClick={(e)=>{
-                        this.eventClick(e);
-                    }}>增加</button>
-        </div>)
-        
-       
+    render(){
+        return (
+            <div>
+                <div>component A,参数是:{this.props.match.params.id}</div>
+                <Switch>
+                 <Route exact path={`${this.props.match.path}`} 
+                        render={(route)=>{
+                            return <div>当前组件是不带参数</div>
+                        }}
+                    />
+                    <Route path={`${this.props.match.path}/:id`} 
+                        render={(route)=>{
+                            return <div>当前组件是带参数：{route.match.params.id}</div>
+                        }}
+                    />
+                </Switch>
+            </div>
+        )
     }
 }
+class B extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>component B</div>
+        )
+    }
+}
+class Wrapper extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+                <Link to="/a/123">组件a</Link>
+                <br/>
+                <Link to="/b">组件b</Link>
+            {this.props.children}
+            </div>
+        )
+    }
+}
+
+
 ReactDOM.render(
-    <Component/>,
+    <Router>
+        <Wrapper>
+            <Route path="/a/:id" component={A}/>
+            <Route path="/b" component={B}/>
+        </Wrapper>
+    </Router>,
     document.getElementById("app")
 );
 
